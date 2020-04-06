20d3c3f2801dc66e6df6941089729c26ce55e942
Przemysław Kondzierski <PKondzierski@gmail.com>
2020-04-06 22:27:05 +0200

    rest+login working everything

--- src/main/webapp/reactjs/src/service/AuthenticationService.js
+++ src/main/webapp/reactjs/src/service/AuthenticationService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'http://localhost:5656'
+const API_URL = 'http://localhost:8080'
 
     export const USER_NAME_SESSION_ATTRIBUTE_NAME = 'authenticatedUser'
 
