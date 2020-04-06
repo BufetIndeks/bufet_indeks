5e4b861b8df925c6e6fc4e32e0492a826b2abfd3
Przemysław Kondzierski <PKondzierski@gmail.com>
2020-04-06 23:30:58 +0200

    Zmiany duze

--- src/main/webapp/reactjs/src/setupProxy.js
+++ src/main/webapp/reactjs/src/setupProxy.js
@@ -1,19 +1,7 @@
-const proxy = require("http-proxy-middleware")
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function (app){
-
-    app.use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "http://localhost:8080"); // update to match the domain you will make the request from
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        next();
-    });
-
-    app.get('/', function(req, res, next) {
-        // Handle the get for this route
-    });
-
-    app.post('/', function(req, res, next) {
-        // Handle the post for this route
-    });
+module.exports = function(app) {
+    app.use(createProxyMiddleware("/basicauth", { target: "http://localhost:8080" }));
+    app.use(createProxyMiddleware("/menu/dishes", { target: "http://localhost:8080" }));
 
 };
