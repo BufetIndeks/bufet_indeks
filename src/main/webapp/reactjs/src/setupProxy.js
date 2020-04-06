32541831e311efabed24b821b5f06ff9a64adb8d
Przemysław Kondzierski <PKondzierski@gmail.com>
2020-04-07 00:05:45 +0200

    Zmiany duze

--- src/main/webapp/reactjs/src/setupProxy.js
+++ src/main/webapp/reactjs/src/setupProxy.js
@@ -2,14 +2,8 @@ var cors = require('cors');
 
 module.exports = function(app) {
     app.use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "http://bufetindeks.duckdns.org:2023"); // update to match the domain you will make the request from
+        res.header("Access-Control-Allow-Origin", "http://localhost:8080"); // update to match the domain you will make the request from
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         next();
-    });
-    app.get('/*', function(req, res, next) {
-
-    });
-
-    app.post('/*', function(req, res, next) {
     });
 };
