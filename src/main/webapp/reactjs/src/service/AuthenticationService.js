0a8a8dbe7f4b7c4d490e51032db7e2646b6ed730
Przemysław Kondzierski <PKondzierski@gmail.com>
2020-04-06 20:17:18 +0200

    rest+login working everything

--- src/main/webapp/reactjs/src/service/AuthenticationService.js
+++ src/main/webapp/reactjs/src/service/AuthenticationService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'http://bufetindeks.duckdns.org:2024'
+const API_URL = 'http://localhost:8080'
 
     export const USER_NAME_SESSION_ATTRIBUTE_NAME = 'authenticatedUser'
 
