4ad54133ffc65253861fb1b28fee89cec9484baa
Przemysław Kondzierski <PKondzierski@gmail.com>
2020-04-07 23:53:04 +0200

    Merge branch 'dev-gk' of https://github.com/BufetIndeks/bufet_indeks
     Conflicts:
    	src/main/webapp/reactjs/src/components/LoginComponent.js
    	src/main/webapp/reactjs/src/components/RegisterComponent.js

--- src/main/webapp/reactjs/src/App.js
+++ src/main/webapp/reactjs/src/App.js
@@ -6,18 +6,19 @@ import MenuComponent from './components/MenuComponent';
 import LogoutComponent from './components/LogoutComponent';
 import AuthenticatedRoute from './components/AuthenticatedRoute';
 import RegisterComponent from './components/RegisterComponent';
-
+import AuthenticationService from './service/AuthenticationService';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import 'materialize-css/dist/css/materialize.min.css';
 
 export default function App() {
+    const isUserLoggedIn = AuthenticationService.isUserLoggedIn();
 return(
 
             <Router>
                     <MenuComponent />
                     <Switch>
                         <Route path="/" exact component={LoginComponent} />
-                        <Route path="/login" exact component={LoginComponent} />
+                        {!isUserLoggedIn && <Route path="/login" exact component={LoginComponent} />}
                         <AuthenticatedRoute path="/logout" exact component={LogoutComponent} />
                         <Route path="/list" exact component={DishList} />
                         <AuthenticatedRoute path="/register" exact component={RegisterComponent} />
