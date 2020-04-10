7f690835a9fef0dd04d563eea4c1513965a5c155
Feargan <38695943+Feargan@users.noreply.github.com>
2020-04-10 14:41:43 +0200

    eject session after logout

--- src/main/java/pl/l3/bufet/security/SecurityConfig.java
+++ src/main/java/pl/l3/bufet/security/SecurityConfig.java
@@ -7,9 +7,17 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -17,18 +25,29 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public static final String ADMIN = "ROLE_ADMIN";
     public static final String TABLE = "ROLE_TABLE";
     public static final String WORKER = "ROLE_WORKER";
+
+    /*public class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
+        public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+                throws IOException, ServletException {
+            super.onLogoutSuccess(request, response, authentication);
+            // handle success here
+        }
+    }*/
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
         http    .cors().and().httpBasic().and()
                 .csrf().disable()
                 .authorizeRequests()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/admin/register").hasRole("ADMIN")
                 .antMatchers("/**").permitAll()
-                .and()
-                .logout()
-
+                .and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).clearAuthentication(true)
+                .invalidateHttpSession(true)
+                //.logoutSuccessHandler(new CustomLogoutSuccessHandler())
+                .deleteCookies("JSESSIONID");
     }
-
 
 
     @Bean
--- src/main/webapp/reactjs/src/components/LogoutComponent.js
+++ src/main/webapp/reactjs/src/components/LogoutComponent.js
@@ -1,6 +1,26 @@
 import React, { Component } from 'react'
+import axios from 'axios';
+import {API_URL} from "../ApiUrl";
+import AuthenticationService from '../service/AuthenticationService';
 
 class LogoutComponent extends Component {
+    constructor(props) {
+        super(props)
+
+
+    }
+
+    componentDidMount() {
+        axios.get(API_URL + "/logoutt")
+            .then((response) => {
+                console.log("ok");
+            })
+            .catch((error) => {
+                console.log("error")
+            })
+        AuthenticationService.logout()
+    }
+
     render() {
         return (
             <>
