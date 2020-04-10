919420f686ac29ed15487536aa324c04eeea5315
PKondzierski <kondzierski.p@gmail.com>
2020-04-10 16:06:28 +0200

    Działający login + logout + estetyka

--- src/main/webapp/reactjs/src/components/LogoutComponent.js
+++ src/main/webapp/reactjs/src/components/LogoutComponent.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react'
+import axios from 'axios';
+import {API_URL} from "../ApiUrl";
+import AuthenticationService from '../service/AuthenticationService';
 
 class LogoutComponent extends Component {
+    constructor(props) {
+        super(props)
+
+
+    }
+
+    componentDidMount() {
+        axios.post(API_URL + "/logout")
+            .then((response) => {
+                console.log("ok");
+            })
+            .catch((error) => {
+                console.log("error")
+            })
+        AuthenticationService.logout()
+        this.props.history.push("")
+    }
+
     render() {
         return (
             <>
-                <h1>You are logged out</h1>
-                <div className="container">
-                    Thank You for Using Our Application.
-                </div>
+
             </>
         )
     }
 }
-
 export default LogoutComponent
