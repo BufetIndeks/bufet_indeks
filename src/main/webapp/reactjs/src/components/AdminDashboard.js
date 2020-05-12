6f92e97c609ebe1638c8ea4e1265c60312e9e702
Gabryjiel <33352685+Gabryjiel@users.noreply.github.com>
2020-05-12 20:43:43 +0200

    Admin dashboard ulepszony

--- src/main/webapp/reactjs/src/components/AdminDashboard.js
+++ src/main/webapp/reactjs/src/components/AdminDashboard.js
@@ -1,18 +1,69 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Grid, Container, Button, Box, Typography } from '@material-ui/core'
 
 const AdminDashboard = props => {
 
     return(
-        <ul className="menuComponent">
-            <Link to="/admin/dishes/new"><li>Dodaj</li></Link>
-            <Link to={{ pathname: `/admin/dishes`, state: {view: 'admin'}}}><li>Usuń</li>></Link>
-            <Link to={{ pathname: `/admin/dishes`, state: {view: 'admin'}}}><li>Edytuj</li>></Link>
-            <Link to="/admin/order"><li>Zamówienia</li>></Link>
-            <Link to="/admin/dailyreport"> <li>Raport dnia</li>></Link>
-            <Link to="/logout"> <li>Raport dnia</li>></Link>
-        </ul>
-    );
+        <Container maxWidth="sm">
+            <Box display="flex" justifyContent="center">
+                <Grid container>
+                    
+                    <Grid item xs={12}>
+                        <Box display="flex" justifyContent="center">
+                            <Typography variant="h4">
+                                Panel Administratora
+                            </Typography>
+                        </Box>
+                    </Grid>
+
+                    <Grid item xs={12}>
+                        <Link to="/admin/dishes/new">
+                            <Button fullWidth color="primary" variant="contained">
+                                Dodaj danie
+                            </Button>
+                        </Link>
+                    </Grid>
+
+                    <Grid item xs={12}>
+                        <Link to={{ pathname: `/admin/dishes`, state: {view: 'admin'}}}>
+                            <Button fullWidth color="primary" variant="contained">
+                                Lista dań
+                            </Button>
+                        </Link>
+                    </Grid>
+
+                    <Grid item xs={12}>
+                        <Link to="/admin/ingredients">
+                            <Button fullWidth color="primary" variant="contained">
+                                Lista składników
+                            </Button>
+                        </Link>
+                    </Grid>
+
+                    <Grid item xs={12}>
+                        <Link to="/admin/categories">
+                            <Button fullWidth color="primary" variant="contained">
+                                Lista kategorii
+                            </Button>
+                        </Link>
+                    </Grid>
+
+                </Grid>
+            </Box>
+        </Container>
+    )
+
+    // return(
+    //     <ul className="menuComponent">
+    //         <Link to="/admin/dishes/new"><li>Dodaj</li></Link>
+    //         <Link to={{ pathname: `/admin/dishes`, state: {view: 'admin'}}}><li>Usuń</li>></Link>
+    //         <Link to={{ pathname: `/admin/dishes`, state: {view: 'admin'}}}><li>Edytuj</li>></Link>
+    //         <Link to="/admin/order"><li>Zamówienia</li>></Link>
+    //         <Link to="/admin/dailyreport"> <li>Raport dnia</li>></Link>
+    //         <Link to="/logout"> <li>Raport dnia</li>></Link>
+    //     </ul>
+    // );
 };
 
 export default AdminDashboard;
