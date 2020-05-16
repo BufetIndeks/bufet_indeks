d9e79b2f693dbea03c7ef0a035485f60d2001bdc
Gabryjiel <33352685+Gabryjiel@users.noreply.github.com>
2020-05-16 13:36:06 +0200

    HomeComponent: Zmiana wyglądu i linku

--- src/main/webapp/reactjs/src/components/HomeComponent.js
+++ src/main/webapp/reactjs/src/components/HomeComponent.js
@@ -1,21 +1,38 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import {Container, Grid} from '@material-ui/core'
+import { Container, Grid, Typography, Box } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles({
+    link: {
+        color: "green",
+        fontFamily: "Roboto",
+        textAlign: 'center'
+    },
+    mt: {
+        marginTop: "10px"
+    }
+})
+
 
 const HomeComponent = props => {
 
+    const classes = useStyles();
+
     return(
         <Container maxWidth="md">
-            <Link to="/">
-                <Grid container justify="center" direction="column" alignItems="center">
-                    <Grid item xs={12}>
-                        <img className="col s12 l4 offset-l4 marginTop" src={require("../logo.svg")} width="300px" alt="Logo Bufet INDEKS"/>
+            <Link to="/menu">
+                    <Grid container direction="column" alignItems="center">
+                        <Grid item xs={12}>
+                            <img className="col s12 l4 offset-l4 marginTop" src={require("../logo.svg")} width="300px" alt="Logo Bufet INDEKS"/>
+                        </Grid>
+                        
+                        <Grid item xs={12} className={classes.mt}>
+                            <Typography variant="h3" component="h3" className={classes.link}>
+                                Dotnij, aby zamówić
+                            </Typography>
+                        </Grid>
                     </Grid>
-                        <h1>Dotnij, aby zamówić</h1>
-                    <Grid item xs={12}>
-
-                    </Grid>
-                </Grid>
             </Link>
         </Container>
     )
