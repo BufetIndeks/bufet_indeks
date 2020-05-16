95839edcb826ba8d4c8fdf4c79a6c59a259a6de8
Gabryjiel <33352685+Gabryjiel@users.noreply.github.com>
2020-05-16 13:37:13 +0200

    MenuComponent: Usunięcie niepotrzebnych linków

--- src/main/webapp/reactjs/src/components/MenuComponent.js
+++ src/main/webapp/reactjs/src/components/MenuComponent.js
@@ -31,26 +31,10 @@ const MenuComponent = props => {
                     <Link to="/" className={classes.buttonlink}>Home</Link>
                     <Link to="/login" className={classes.buttonlink}>Zaloguj</Link>
                     <Link to="/logout" className={classes.buttonlink}>Logout</Link>
-                    <Link to={{pathname: "/list", state: { view: 'client'}}} className={classes.buttonlink}>Lista dań</Link>
                     <Link to="/admin/dashboard" className={classes.buttonlink}>Dashboard</Link>
-                    <Link to="/allergens" className={classes.buttonlink}>Składniki</Link>
                 </Toolbar>
             </AppBar>
         </header>
-
-        // <header>
-        //     <nav className="green lighten-1">
-        //         <ul>
-        //             <Link className="nav-link" to="/">Home</Link>
-        //             <Link className="nav-link" to="/login">Zaloguj</Link>
-        //             <Link className="nav-link" to="/admin/register">Rejestracja</Link>
-        //             <Link className="nav-link" to="/logout" >Logout</Link>
-        //             <Link className="nav-link" to={{pathname: "/list", state: { view: 'client'}}}>Lista dań</Link>
-        //             <Link className="nav-link" to="/admin/dashboard">Dashboard</Link>
-        //             <Link className="nav-link" to="/allergens">Składniki</Link>
-        //         </ul>
-        //     </nav>
-        // </header>
     )
     
 }
